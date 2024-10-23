c6f60c54d86ca2a2b8d195eed1fc69e135fbcef2
Yen <haiyenvn22@gmail.com>
2024-10-23 22:49:57 +0700

    Update dashboard

--- src/pages/dashboard/components/carousel-home.tsx
+++ src/pages/dashboard/components/carousel-home.tsx
@@ -6,23 +6,33 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import React from "react";
+import Autoplay from "embla-carousel-autoplay"
 
 const slide = [
-  'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/08/do_an_tot_nghiep_dai_hoc_fpt_10-1661503315.jpeg',
+  'https://dnuni.fpt.edu.vn/wp-content/uploads/2022/08/DSC02394.jpg',
   'https://dnuni.fpt.edu.vn/wp-content/uploads/2022/04/IMG_0454-1350x900.jpg',
   'https://i.chungta.vn/2022/05/09/ugu9850-1652081269_1200x0.jpg',
 ];
 
 export default function CarouselHome() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
   return (
+    
     <div className="w-full pl-10 pr-10">
-      <Carousel>
+       <Carousel
+      plugins={[plugin.current]}
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
       <CarouselContent>
         {slide.map((image, index) => (
           <CarouselItem key={index}>
       
               <Card>
-                <CardContent className="flex aspect-[4/3] p-0">
+                <CardContent className="flex aspect-[6/4] p-0">
                   <img src={image} alt={`Slide ${index + 1}`} />
                 </CardContent>
               </Card>
