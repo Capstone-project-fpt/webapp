9de999e80e1dd1b8b556cdc8a989a33ca6a70aaf
Ly Le <lecongly28102002@gmail.com>
2024-11-09 15:54:21 +0700

    feat: 🎸 evaluations detail page

--- src/pages/evaluation-committee/group-detail/index.tsx
+++ src/pages/evaluation-committee/group-detail/index.tsx
@@ -1,35 +1,64 @@
 import { LoadingTableLottie } from "@/components";
 import ErrorBoundaryComponent from "@/components/error/error-boundary";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useGetEvaluationQuery } from "@/store/api/v1/endpoints/evaluations";
 import { setBreadCrumb } from "@/store/slice/app";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
+import Calendar from "./calendar";
+import Peoples from "./peoples";
+
+const TABS = [
+  { name: "calendar", label: "Calendar", component: Calendar },
+  { name: "peoples", label: "Peoples", component: Peoples },
+];
+
+const TABS_NAMES = TABS.reduce((acc, tab) => {
+  acc[tab.name] = tab.label;
+  return acc;
+}, {} as Record<string, string>);
 
 const EvaluationDetail: React.FC = () => {
-  const { evaluationId } = useParams<{ evaluationId: string}>();
+  const { evaluationId, tab } = useParams<{
+    evaluationId: string;
+    tab?: string;
+  }>();
 
   const {
     data: evaluationData,
     isLoading,
     error,
-  } = useGetEvaluationQuery({ id: Number(evaluationId) }, { skip: !evaluationId });
+  } = useGetEvaluationQuery(
+    { id: Number(evaluationId) },
+    { skip: !evaluationId }
+  );
 
   const evaluation = evaluationData?.data;
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [currentTab, setCurrentTab] = useState(tab || "calendar");
+
+  const handleTabChange = (tab: string) => {
+    setCurrentTab(tab);
+    navigate(`/evaluation-committees/${evaluationId}/${tab}`);
+  };
 
   useEffect(() => {
     const breadcrumb = [
       { title: "Home", link: "/" },
-      { title: "Evaluations", link: "/Evaluations" },
+      { title: "Evaluations", link: "/evaluation-committees" },
       {
         title: `${evaluation?.name || "Evaluation " + evaluationId}`,
-        link: `/Evaluations/${evaluationId}`,
+        link: `/evaluation-committees/${evaluationId}`,
+      },
+      {
+        title: `${TABS_NAMES[currentTab]}`,
+        link: `/evaluation-committees/${evaluationId}/${currentTab}`,
       },
     ];
     dispatch(setBreadCrumb(breadcrumb));
-  }, [ dispatch, evaluation, evaluationId]);
+  }, [currentTab, dispatch, evaluation, evaluationId]);
 
   if (isLoading) {
     return (
@@ -49,7 +78,25 @@ const EvaluationDetail: React.FC = () => {
     }
     return (
       <div>
-        Done
+        <Tabs defaultValue={currentTab} onValueChange={handleTabChange}>
+          <TabsList>
+            {TABS.map((tab) => (
+              <TabsTrigger
+                key={tab.name}
+                value={tab.name}
+                className="lg:w-[150px] w-full"
+              >
+                {tab.label}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          {TABS.map((tab) => (
+            <TabsContent key={tab.name} value={tab.name} className="mt-4">
+              <tab.component />
+            </TabsContent>
+          ))}
+        </Tabs>
       </div>
     );
   }
