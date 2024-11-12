b0069273d5e7f8cd638b9bb12b5024f28263e752
minhdev313 <voducminh313@gmail.com>
2024-11-12 21:35:32 +0700

    Update view detail

--- src/pages/evaluation-committee/components/actions.tsx
+++ src/pages/evaluation-committee/components/actions.tsx
@@ -2,21 +2,15 @@ import { ActionCell } from "@/components/data-table";
 import { EvaluationType } from "@/types/evaluation";
 import { Row } from "@tanstack/react-table";
 import React, { useState } from "react";
-// import CreateUpdateDialog from "./create-update-dialog";
 import DeleteDialog from "./delete-dialog";
-
+import { useNavigate } from "react-router-dom";
 
 const Actions: React.FC<{ row: Row<EvaluationType> }> = ({ row }) => {
-  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+  const navigate = useNavigate();
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   return (
     <>
-      {/* <CreateUpdateDialog
-        student={row.original}
-        open={isUpdateModalOpen}
-        onOpenChange={setIsUpdateModalOpen}
-      /> */}
       <DeleteDialog
         group={row.original}
         open={isDeleteModalOpen}
@@ -25,8 +19,9 @@ const Actions: React.FC<{ row: Row<EvaluationType> }> = ({ row }) => {
       <ActionCell
         items={[
           {
-            item: "View Detail",
+            item: "View Details",
             onClick: () => {
+              navigate(`/evaluation-committees/${row.original.id}`);
             },
           },
           "-",
