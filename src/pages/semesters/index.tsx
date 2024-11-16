85de4916b4c6d1c8a19872a58e02b030462f3bd4
minhdev313 <voducminh313@gmail.com>
2024-11-16 19:13:12 +0700

    Create search-semester

--- src/pages/semesters/index.tsx
+++ src/pages/semesters/index.tsx
@@ -5,9 +5,14 @@ import { GoPlus } from "react-icons/go";
 import { useDispatch } from "react-redux";
 import CreateUpdateDialog from "./components/create-update-dialog";
 import { SemesterTable } from "./components/semester-table";
+import { Label } from "@/components/ui/label";
+import SearchBar from "@/components/common/search-bar";
 
 const Semesters: React.FC = () => {
   const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+
   useEffect(() => {
     dispatch(
       setBreadCrumb([
@@ -17,27 +22,32 @@ const Semesters: React.FC = () => {
     );
   }, [dispatch]);
 
-  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
-
   return (
     <>
-      <div className="flex justify-end mb-2">
-        <Button
-          variant="outline"
-          className="ml-1"
-          onClick={() => setIsCreateModalOpen(true)}
-        >
-          <GoPlus className="h-4 w-4" />
-        </Button>
+      <div>
+        <div className="flex justify-between items-center mb-2">
+          <div className="flex flex-col gap-2 mb-4">
+            <Label className="text-base font-medium text-gray-700">
+              Search Semester
+            </Label>
+            <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+          </div>
+          <Button
+            variant="outline"
+            className="ml-1"
+            onClick={() => setIsCreateModalOpen(true)}
+          >
+            <GoPlus className="h-4 w-4" />
+          </Button>
+        </div>
         {isCreateModalOpen && (
           <CreateUpdateDialog
             open={isCreateModalOpen}
             onOpenChange={setIsCreateModalOpen}
           />
         )}
+        <SemesterTable searchKey={searchTerm} />
       </div>
-
-      <SemesterTable />
     </>
   );
 };
