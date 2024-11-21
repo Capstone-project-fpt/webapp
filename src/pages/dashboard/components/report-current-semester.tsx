fc152e2172450a82670e59b88982d328223ab398
yen <haiyenvn22@gmail.com>
2024-11-21 20:53:44 +0700

    fix/remove total students

--- src/pages/dashboard/components/report-current-semester.tsx
+++ src/pages/dashboard/components/report-current-semester.tsx
@@ -1,9 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { RootState } from "@/store";
-import { useGetUsersQuery } from "@/store/api/v1/endpoints/admin";
 import { useGetEvaluationsQuery } from "@/store/api/v1/endpoints/evaluations";
 import { useGetGroupsQuery } from "@/store/api/v1/endpoints/groups";
-import { UserTypes } from "@/types/accounts";
 import React from "react";
 import { useSelector } from "react-redux";
 
@@ -12,21 +10,19 @@ const ReportStatis: React.FC = () => {
     const currentSemester = useSelector(
         (state: RootState) => state.resource.currentSemester
     );
-    const { data: studentData, error: studentError, isLoading: isLoadingStudents } = useGetUsersQuery({
-        user_types: UserTypes.STUDENT,
-    })
     const { data: groupData, error: groupError, isLoading: isLoadingGroups } = useGetGroupsQuery({
+        limit: 1, page: 1,
         semester_id: currentSemester?.id,
     });
 
     const { data: evaluationData, error: evaluationError, isLoading: isLoadingEvaluations } = useGetEvaluationsQuery({
-
+        limit: 1, page: 1,
         semester_id: currentSemester?.id,
     });
 
 
-    const isLoading = isLoadingGroups || isLoadingEvaluations || isLoadingStudents;
-    const error = groupError || evaluationError || studentError;
+    const isLoading = isLoadingGroups || isLoadingEvaluations ;
+    const error = groupError || evaluationError ;
 
 
     if (isLoading) {
@@ -41,15 +37,6 @@ const ReportStatis: React.FC = () => {
         <div className="flex flex-col md:flex-row md:justify-between gap-4">
             <Card className="flex-1">
                 <CardHeader>
-                    <CardTitle>Total Students</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <p className="text-2xl font-bold">{studentData?.data?.meta?.total}</p>
-                </CardContent>
-            </Card>
-
-            <Card className="flex-1">
-                <CardHeader>
                     <CardTitle>Total Capstone Groups</CardTitle>
                 </CardHeader>
                 <CardContent>
@@ -59,7 +46,7 @@ const ReportStatis: React.FC = () => {
 
             <Card className="flex-1">
                 <CardHeader>
-                    <CardTitle>Total Evaluators</CardTitle>
+                    <CardTitle>Total Evaluation Commitee </CardTitle>
                 </CardHeader>
                 <CardContent>
                     <p className="text-2xl font-bold">{evaluationData?.data?.meta?.total}</p>
