6d085737d7570239b0e51c2023dab77210458fe7
Ly Le <lecongly28102002@gmail.com>
2024-11-30 21:01:55 +0700

    refactor: 💡 remove unused calendar code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
-import { Appointment } from "@/models";
 import { type ClassValue, clsx } from "clsx";
 import {
-  differenceInDays,
   eachDayOfInterval,
   eachHourOfInterval,
   eachMonthOfInterval,
@@ -10,11 +8,6 @@ import {
   endOfMonth,
   endOfYear,
   format,
-  getWeekOfMonth,
-  isSameDay,
-  isSameMonth,
-  isSameWeek,
-  isWithinInterval,
   startOfDay,
   startOfMonth,
   startOfYear
@@ -82,53 +75,6 @@ export const calculateNewDates = (viewMode: string, index: number, currentIndex:
   return { start, end };
 };
 
-
-export const filterAppointments = (
-  appt: Appointment,
-  index: number,
-  dateRange: DateRange,
-  viewMode: string,
-): boolean => {
-  const apptDate = new Date(appt.start);
-  if (
-    !dateRange.from ||
-    !dateRange.to ||
-    !isWithinInterval(apptDate, { start: dateRange.from, end: dateRange.to })
-  ) {
-    return false;
-  }
-  return isAppointmentInSlot(apptDate, index, viewMode, dateRange);
-};
-// Helper function to determine if an appointment should be displayed in a specific slot
-const isAppointmentInSlot = (
-  apptDate: Date,
-  index: number,
-  viewMode: string,
-  dateRange: DateRange,
-): boolean => {
-  if (!dateRange.from) return false;
-
-  switch (viewMode) {
-    case "day":
-      return (
-        apptDate.getHours() === index && isSameDay(apptDate, dateRange.from)
-      );
-    case "week":
-      return (
-        apptDate.getDay() - (6 - differenceInDays(new Date(dateRange.to!), new Date(dateRange.from))) === index &&
-        isSameWeek(apptDate, dateRange.from)
-      );
-    case "month":
-      return (
-        getWeekOfMonth(apptDate) === index &&
-        isSameMonth(apptDate, dateRange.from)
-      );
-    case "year":
-      return apptDate.getMonth() === index;
-    default:
-      return false;
-  }
-};
 
 export const getLabelsForView = (viewMode: 'day' | 'week' | 'month' | 'year', dateRange: { start: Date; end: Date }): string[] => {
   switch (viewMode) {
