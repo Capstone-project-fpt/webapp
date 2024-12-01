75509c7d2d7a98289b9e7fa1c7761f153e99c165
Ly Le <lecongly28102002@gmail.com>
2024-12-01 22:22:48 +0700

    feat: 🎸 authorization for CRUD verifier topic

--- src/pages/semesters/semester-detail/decentralization.tsx
+++ src/pages/semesters/semester-detail/decentralization.tsx
@@ -26,6 +26,8 @@ import { UserItem } from "@/types/accounts";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import SelectLecture from "../components/select-lecture";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
 
 const DeleteTeacherDialog: React.FC<{
   open: boolean;
@@ -44,7 +46,7 @@ const DeleteTeacherDialog: React.FC<{
       }).unwrap();
 
       toast({
-        duration: 1000,
+        duration: 3000,
         title: "Delete lecturer from verifier topic",
         description:
           res.data || "Teacher removed from verifier topic successfully.",
@@ -52,7 +54,7 @@ const DeleteTeacherDialog: React.FC<{
       onOpenChange(false);
     } catch (error) {
       toast({
-        duration: 1000,
+        duration: 3000,
         variant: "destructive",
         title: "Delete lecturer from verifier topic",
         description:
@@ -99,6 +101,7 @@ const AddTeacherDialog: React.FC<{
           teacher_id: selectedLecture.value.extra_info.teacher.teacher_id,
         }).unwrap();
         toast({
+          duration: 3000,
           title: "Assign lecturer to verification topic",
           description:
             assignData.data ||
@@ -107,7 +110,7 @@ const AddTeacherDialog: React.FC<{
         onOpenChange(false);
       } catch (error) {
         toast({
-          duration: 1000,
+          duration: 3000,
           variant: "destructive",
           title: "Assign lecturer to verification topic",
           description:
@@ -152,6 +155,9 @@ const Decentralization: React.FC = () => {
     error,
     isLoading,
   } = useGetTopicVerifiersQuery({ semester_id: Number(semesterId!) });
+  const currentSemester = useSelector(
+    (state: RootState) => state.resource.currentSemester,
+  );
   const openDeleteDialog = (teacherId: number) => {
     setIsAddModalOpen(teacherId);
     setIsDeleteModalOpen(true);
@@ -181,14 +187,19 @@ const Decentralization: React.FC = () => {
       <SettingCard
         title="Verifier Topic"
         actions={
-          <Button onClick={() => setIsAddTeacherModalOpen(true)}>
-            Add Lecturer
-          </Button>
+          currentSemester?.id === Number(semesterId) && (
+            <Button onClick={() => setIsAddTeacherModalOpen(true)}>
+              Add Lecturer
+            </Button>
+          )
         }
       >
         {!queryData || !queryData.data || !queryData.data.length ? (
           <div>
-            <EmptyResources title="No topic verifier yet"></EmptyResources>
+            <EmptyResources title="No topic verifier yet"
+              content="There are currently no topic verifiers in this semester."
+              shape="empty-contact"
+            ></EmptyResources>
           </div>
         ) : (
           <Table>
@@ -208,7 +219,7 @@ const Decentralization: React.FC = () => {
                     <Avatar>
                       <AvatarImage
                         src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
-                          member.name
+                          member.name,
                         )}&size=32`}
                         alt={member.name}
                       />
@@ -234,6 +245,7 @@ const Decentralization: React.FC = () => {
                           item: "Delete",
                           danger: true,
                           onClick: () => openDeleteDialog(member.id),
+                          isDisable: currentSemester?.id !== Number(semesterId),
                         },
                       ]}
                     />
