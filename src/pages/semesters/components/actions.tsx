477b7a7f14a874300b030ce338bce056ab0123b1
Ly Le <lecongly28102002@gmail.com>
2024-12-04 23:24:28 +0700

    feat: 🎸 authorzation actions semester

--- src/pages/semesters/components/actions.tsx
+++ src/pages/semesters/components/actions.tsx
@@ -5,8 +5,12 @@ import React, { useState } from "react";
 import CreateUpdateDialog from "./create-update-dialog";
 import DeleteDialog from "./delete-dialog";
 import { useNavigate } from "react-router-dom";
+import { RootState } from "@/store";
+import { useSelector } from "react-redux";
+import { UserTypes } from "@/types/accounts";
 
 const Actions: React.FC<{ row: Row<SemesterType> }> = ({ row }) => {
+  const currentUser = useSelector((state: RootState) => state.auth.user);
   const navigate = useNavigate();
   const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
@@ -14,34 +18,35 @@ const Actions: React.FC<{ row: Row<SemesterType> }> = ({ row }) => {
   return (
     <>
       <CreateUpdateDialog
-        semester={row.original}
-        open={isUpdateModalOpen}
-        onOpenChange={setIsUpdateModalOpen}
+      semester={row.original}
+      open={isUpdateModalOpen}
+      onOpenChange={setIsUpdateModalOpen}
       />
       <DeleteDialog
-        semester={row.original}
-        open={isDeleteModalOpen}
-        onOpenChange={setIsDeleteModalOpen}
+      semester={row.original}
+      open={isDeleteModalOpen}
+      onOpenChange={setIsDeleteModalOpen}
       />
       <ActionCell
-        items={[
-          {
-            item: "View Details",
-            onClick: () => {
-              navigate(`/semesters/${row.original.id}`);
-            },
-          },
-          {
-            item: "Edit",
-            onClick: () => setIsUpdateModalOpen(true),
-          },
-          "-",
-          {
-            item: "Delete",
-            danger: true,
-            onClick: () => setIsDeleteModalOpen(true),
-          },
-        ]}
+      items={[
+        {
+        item: "View Details",
+        onClick: () => {
+          navigate(`/semesters/${row.original.id}`);
+        },
+        },
+        ...(currentUser?.common_info.user_type === UserTypes.ADMIN ? [
+        {
+          item: "Edit",
+          onClick: () => setIsUpdateModalOpen(true),
+        },
+        {
+          item: "Delete",
+          danger: true,
+          onClick: () => setIsDeleteModalOpen(true),
+        },
+        ] : []),
+      ]}
       />
     </>
   );
